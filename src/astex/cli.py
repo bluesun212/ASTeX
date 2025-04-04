9a022f2e8fbbae7bfa658ff103aeac9d8489b755
Carsten Knoll <carsten.knoll@posteo.de>
2025-04-04 12:52:20 +0200

    feat: add basic cli functionality for demacro + simple test
    # Last 3 commits:
    
    # 798406c make `pip install -e .` work 
    # 96bdab6 refactor: move package dir into `src/` 
    # 8d2bb46 refactor: add pyproject.toml

--- src/astex/cli.py
+++ src/astex/cli.py
@@ -1,2 +1,41 @@
+import os
+from astex.ast import to_ast
+from .demacro import Demacro
+
+import argparse
+
+# used for debugging
+from ipydex import IPS, activate_ips_on_exception
+
+activate_ips_on_exception()
+
+
 def demacro():
-    print("Successfully executed this function from the command line")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="input file path")
+    parser.add_argument("output", help="input file path")
+
+    parser.add_argument("-rm", "--remove-macros", help="space-separated list of macros to remove", nargs="+")
+
+    args = parser.parse_args()
+
+    doc = to_ast(file=args.input)
+    dm = Demacro()
+
+    # Note: this currently ony handles simple macros like
+    # \newcommand{\tcblue}[1]{\textcolor{blue}{#1}}
+
+    macro_items = []
+    for macro_name in args.remove_macros:
+        macro_items.append((macro_name, {"body": "#1", "args": 1}))
+
+        dm.add_macros(dict(macro_items))
+
+    doc_dm = dm.demacro(doc)
+
+    if os.path.exists(args.output):
+        raise FileExistsError(args.output)
+    with open(args.output, "w") as fp:
+        fp.write(str(doc_dm))
+
+    print(f"File written: {args.output}")
---
+++ test/test_cli.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import unittest
+
+
+current_dir = os.path.dirname(os.path.abspath(sys.modules.get(__name__).__file__))
+
+TEST_DATA_DIR = os.path.join(current_dir, "testdata")
+
+DOC1 = os.path.join(TEST_DATA_DIR, "doc1.tex")
+
+
+class Test_01_CLI(unittest.TestCase):
+
+    def setUp(self):
+        self.files_ro_remove = []
+
+
+    def tearDown(self):
+
+        for fpath in self.files_ro_remove:
+            if os.path.isfile(fpath):
+                os.unlink(fpath)
+        return super().tearDown()
+
+    def test_01_demacro(self):
+
+        def perform_test(macro_string, res_idx):
+            res_path = DOC1.replace(".tex", f"_dm{res_idx}.tex")
+            expected_res_path = DOC1.replace(".tex", f"__dm_expected{res_idx}.tex")
+            cmd = f"demacro {DOC1} {res_path} -rm {macro_string}"
+            return_code = os.system(cmd)
+            self.assertEqual(return_code, 0)
+            self.files_ro_remove.append(res_path)
+
+            with open(res_path) as fp:
+                res_src = fp.read()
+
+            with open(expected_res_path) as fp:
+                expected_res_src = fp.read()
+
+            self.assertEqual(res_src, expected_res_src)
+
+        perform_test(macro_string="tcblue", res_idx=1)
